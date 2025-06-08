355ea08c57316751e37da4ff4be3a8a18e37ce14
Erlin Meutia <erlinmeutiaf@gmail.com>
2025-06-08 11:34:17 +0700

    fix: add ssl.rejectunauthorized=false to prevent ETIMEDOUT

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {} // ✅ Railway butuh ini agar koneksi aman
+    ssl: {
+        rejectUnauthorized: false 
+    } // ✅ Railway butuh ini agar koneksi aman
 });
 
 
